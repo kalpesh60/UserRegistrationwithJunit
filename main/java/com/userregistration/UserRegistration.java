32ffe04d89154659184e91d0e0d9a4a0ff8feaad
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-05-07 15:46:19 +0530

    Added:file of parametrized Junit of email

--- main/java/com/userregistration/UserRegistration.java
+++ main/java/com/userregistration/UserRegistration.java
@@ -4,8 +4,8 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     private static final String namePattern = "^[A-Z]{1}[a-zA-Z]{3,}";
-    private static final String passwordPattern = "^[A-Z]{1}[a-zA-Z]{3,}";
-    private static final String emailPattern = "^[A-Z]{1}[a-zA-Z]{3,}";
+    private static final String passwordPattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%*]{1})(?=.*[a-z]){8,}.*";
+    private static final String emailPattern = "(^[A-Za-z0-9]{1,}[-+_.]{0,1})[A-Za-z0-9]{0,}[@]{1}[A-Za-z0-9]{1,}[.]{1}[A-Za-z]{2,3}[.]{0,1}[A-Za-z]{0,3}";
     private static final String phoneNoPattern = "^[0-9]{2}\\s{1}[0-9]{10}$";
 
     public boolean firstNameValidation(String firstname) {
@@ -18,7 +18,7 @@ public class UserRegistration {
         return pattern.matcher(lastname).matches();
     }
 
-    public boolean emailValidation(String email) {
+    public static boolean emailValidation(String email) {
         Pattern pattern = Pattern.compile(emailPattern);
         return pattern.matcher(email).matches();
     }
--- test/java/UserRegistrationTest.java
+++ test/java/UserRegistrationTest.java
@@ -1,6 +1,6 @@
 import com.userregistration.UserRegistration;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class UserRegistrationTest {
 
@@ -8,69 +8,69 @@ public class UserRegistrationTest {
     public void givenFirstName_WhenValid_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.firstNameValidation("Kailash");
-        Assertions.assertTrue(result);
+        Assert.assertTrue(result);
     }
 
     @Test
-    void givenFirstName_WhenNotValid_ShouldReturnFalse() {
+    public void givenFirstName_WhenNotValid_ShouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.firstNameValidation("ka");
-        Assertions.assertFalse(result);
+        Assert.assertFalse(result);
     }
 
     @Test
-    void givenLastName_WhenValid_ShouldReturnTrue() {
+    public void givenLastName_WhenValid_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.lastNameValidation("Dale");
-        Assertions.assertTrue(result);
+        Assert.assertTrue(result);
     }
 
     @Test
-    void givenLastName_WhenNotValid_ShouldReturnTrue() {
+    public void givenLastName_WhenNotValid_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.lastNameValidation("dh");
-        Assertions.assertFalse(result);
+        Assert.assertFalse(result);
     }
 
     @Test
     void givenEmail_WhenValid_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.emailValidation("kalp.gh@mail.com");
-        Assertions.assertTrue(result);
+        Assert.assertTrue(result);
     }
 
     @Test
     void givenEmail_WhenNotValid_ShouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.emailValidation("kalp.@@com");
-        Assertions.assertFalse(result);
+        Assert.assertFalse(result);
     }
 
     @Test
     void givenPhoneNo_WhenValid_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.phoneNoValidation("91 8892829299");
-        Assertions.assertTrue(result);
+        Assert.assertTrue(result);
     }
 
     @Test
     void givenPhoneNo_WhenNotValid_ShouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.phoneNoValidation("875456666");
-        Assertions.assertFalse(result);
+        Assert.assertFalse(result);
     }
 
     @Test
     void givenPassword_WhenValid_ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.passwordValidation("Asdf@12344");
-        Assertions.assertTrue(result);
+        Assert.assertTrue(result);
     }
 
     @Test
     void givenPassword_WhenNotValid_ShouldReturnFalse() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.passwordValidation("adgggghh");
-        Assertions.assertFalse(result);
+        boolean result = userRegistration.passwordValidation("dddddd");
+        Assert.assertFalse(result);
     }
 }
