7272c86f3481765803405ad69fa72fd33b6c3ae9
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-05-11 14:35:17 +0530

    Refactor:Added code to use Lambda Expressions

--- main/java/com/userregistration/UserRegistration.java
+++ main/java/com/userregistration/UserRegistration.java
@@ -1,14 +1,21 @@
 package com.userregistration;
+
 import java.util.regex.Pattern;
 
+@FunctionalInterface
+interface UserRegistrationLambda {
+    boolean matches(String pattern,String input);
+}
+
 public class UserRegistration {
+    UserRegistrationLambda matchInput = ((pattern, input) -> Pattern.compile(pattern).matcher(input).matches());
     private static final String namePattern = "^[A-Z]{1}[a-zA-Z]{3,}";
     private static final String passwordPattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%*]{1})(?=.*[a-z]){8,}.*";
     private static final String emailPattern = "(^[A-Za-z0-9]{1,}[-+_.]{0,1})[A-Za-z0-9]{0,}[@]{1}[A-Za-z0-9]{1,}[.]{1}[A-Za-z]{2,3}[.]{0,1}[A-Za-z]{0,3}";
     private static final String phoneNoPattern = "^[0-9]{2}\\s{1}[0-9]{10}$";
 
     public boolean firstNameValidation(String firstname) throws UserRegistrationException {
-        boolean result = Pattern.compile(namePattern).matcher(firstname).matches();
+        boolean result = matchInput.matches(namePattern,firstname);
         if(result) {
             System.out.println("name is valid");
             return true;
@@ -19,7 +26,7 @@ public class UserRegistration {
     }
 
     public boolean lastNameValidation(String lastname) throws UserRegistrationException {
-        boolean result = Pattern.compile(namePattern).matcher(lastname).matches();
+        boolean result = matchInput.matches(namePattern,lastname);
         if(result) {
             System.out.println("Last name is valid");
             return true;
@@ -29,8 +36,8 @@ public class UserRegistration {
         }
     }
 
-    public static boolean emailValidation(String email) throws UserRegistrationException {
-        boolean result = Pattern.compile(emailPattern).matcher(email).matches();
+    public boolean emailValidation(String email) throws UserRegistrationException {
+        boolean result = matchInput.matches(emailPattern,email);
         if(result) {
             System.out.println("email is valid");
             return true;
@@ -41,7 +48,7 @@ public class UserRegistration {
     }
 
     public boolean phoneNoValidation(String phonenumber) throws UserRegistrationException {
-        boolean result = Pattern.compile(phoneNoPattern).matcher(phonenumber).matches();
+        boolean result = matchInput.matches(phoneNoPattern,phonenumber);
         if(result) {
             System.out.println("phone number is valid");
             return true;
@@ -52,7 +59,7 @@ public class UserRegistration {
     }
 
     public boolean passwordValidation(String password) throws UserRegistrationException {
-        boolean result = Pattern.compile(passwordPattern).matcher(password).matches();
+        boolean result = matchInput.matches(passwordPattern,password);
         if(result) {
             System.out.println("password is valid");
             return true;
